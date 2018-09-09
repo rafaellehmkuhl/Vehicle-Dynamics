f15f1747ac01bea843ef9ee4cc6f5d01d454424e
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2018-09-09 04:57:16 -0300

    New routine with loop for inclination increment
    New routine evaluates the velocity plot for variable inclinations, from 0 to 50%.

--- longitudinal_and_slope_equations.py
+++ longitudinal_and_slope_equations.py
@@ -14,77 +14,78 @@ Af = 2 # Area frontal do carro
 hp = 1 # Altura do centro de pressao aerodinamica
 hcg = 1 # Altura do centro de gravidade
 incl = 0 # Inclinaçao percentual da rampa
-theta = math.atan(incl/100) # Angulaçao da rampa
 g = 9.81 # Gravidade
 l1 = 1 # Distancia do eixo dianteiro em ralaçao ao Cg
 l2 = 2.1 # Distancia do eixo traseiro em ralaçao ao Cg
-
-# Calculate the three constant therms
-a = (
-    (rho*Cx*Af*hp*ff + Af*hp*fr)/(2*l1+l2) -
-    (rho*Af*Cx)/2
-    )
-b = (
-    Fxf +
-    ff * (m*g*hcg*math.sin(theta) - m*g*l2*math.cos(theta))/(l1+l2) +
-    Fxr -
-    (m*g*math.sin(theta)) +
-    fr * (m*g*hcg*math.sin(theta) + m*g*l2*math.cos(theta))/(l1+l2)
-    )
-c = (
-    m -
-    m*hcg * ((ff+fr)/(l1+l2))
-    )
-
-# Print a, b and c so we can know if they have ok values
-print('a: ' + str(a))
-print('b: ' + str(b))
-print('c: ' + str(c))
 ff = 0.04 # Fator de atrito das rodas dianteiras
 fr = 0.04 # Fator de atrito das rodas traseiras
 
 def delta_v(a, b, c, v, dt):
-    # Divide the increment in time "dt" by 1000 to transform from milliseconds to seconds
-    dt = dt/1000
-    # Calculate the increment in velocity "dv" as a function of the previous velocity and the increment in time
-    dv = ((v**2*a+b)/c)*dt
-    return dv
+        # Calculate the increment in velocity "dv" as a function of the previous velocity and the increment in time
+        dv = ((v**2*a+b)/c)*dt
+        return dv
 
-# Create vectors (Python's "list") to store the values of time and velocity
-time = []
-vel = []
-acel = []
+for incl in range(0, 50, 3):
+    # Calculate inclination angle
+    theta = math.atan(incl/100)
 
-# Initialize variables for (previous and current) velocity and for time increment
-old_v = 0
-new_v = 0
-dt = 1 # milliseconds
+    # Calculate the three constant therms
+    a = (
+        + (ff+fr) * (rho*Cx*Af*hp) / (2*(l1+l2))
+        - (rho*Af*Cx) / 2
+        )
+    b = (
+        + Fxf
+        + ff * (m*g*hcg*math.sin(theta) - m*g*l1*math.cos(theta)) / (l1+l2)
+        + Fxr
+        + fr * (m*g*hcg*math.sin(theta) + m*g*l2*math.cos(theta)) / (l1+l2)
+        - m*g*math.sin(theta)
+        )
+    c = (
+        + m
+        - m*hcg * (ff+fr) / (l1+l2)
+        )
 
-# Time and Velocity increment Loop
-# Increment the time in steps of 1 millisecond
-for t_inst in range(0, 500000, dt):
-    # Increment in time is calculated by the delta_v function
-    dv = delta_v(a, b, c, old_v, dt)
-    # Current velocity equals the previous velocity plus the numerical velocity increment in time
-    new_v = old_v + dv
-    # Updates previous velocity value with the new velocity value, so it can be incremented on the next iteration
-    old_v = new_v
-    # Append (include) time and velocity values on their lists (vectors)
-    time.append(t_inst/1000)
-    vel.append(new_v*3.6)
-    acel.append(dv/dt)
+    # Create vectors (Python's "list") to store the values of time and velocity
+    time = []
+    accel = []
+    vel = []
+    dist = []
 
-# Create Time x Velocity plot
-plt.subplot(211)
-plt.plot(time, vel)
-plt.title('Velocidade')
-plt.grid(True)
+    # Initialize variables for (previous and current) velocity and for time increment
+    old_v = 0
+    dt = 0.001 # seconds
+    t_inst = 0 # Instantaneous time for the loop, in seconds
 
-# Create Time x Velocity plot
-plt.subplot(212)
-plt.plot(time, acel)
-plt.title('Aceleraçao')
-plt.grid(True)
+    _accel = 1 # Acceleration [m/s^2]
+    _vel = 0
+    _dist = 0 # Distance value [m]
+
+    # Time and Velocity increment Loop
+    # Increment the time in steps of 1 millisecond
+    while _accel > 0.00001:
+        # Increment time
+        t_inst = t_inst + dt
+        # Increment in time is calculated by the delta_v function
+        dv = delta_v(a, b, c, old_v, dt)
+        # Current velocity equals the previous velocity plus the numerical velocity increment in time
+        _vel = old_v + dv
+        # Updates acceleration value
+        _accel = dv/dt
+        # Updates distance value
+        _dist = _dist + _vel * dt
+        # Updates previous velocity value with the new velocity value, so it can be incremented on the next iteration
+        old_v = _vel
+        # Append (include) time and velocity values on their lists (vectors)
+        time.append(t_inst)
+        vel.append(_vel*3.6)
+        accel.append(_accel)
+        dist.append(_dist)
 
 # Show plots
+plt.title('Velocidade x Tempo')
+plt.xlabel('Tempo [s]')
+plt.ylabel('Velocidade [km/h]')
+plt.grid(True)
+plt.legend(title='Inclinaçao')
 plt.show()
