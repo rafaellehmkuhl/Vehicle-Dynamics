f540186ee6cee9adf57520aca643cd5eeb243b29
Unknown <rafael.lehmkuhl93@gmail.com>
2018-11-25 22:30:16 -0200

    Update code and add Jupyter notebook for plotting and analysing

--- trapezoidal_direction_mechanism/lets_FUCK_freudenstein_opt.py
+++ trapezoidal_direction_mechanism/lets_FUCK_freudenstein_opt.py
@@ -2,14 +2,27 @@ from math import *
 import scipy.optimize as opt
 from mpmath import cot, acot
 import numpy as np
+import pandas as pd
+
+df = pd.DataFrame()
+
+_index = []
+_radius = []
+_input = []
+_ackerman = []
+_freudenstein = []
+_error = []
+_a = []
+_b = []
+_beta = []
+_d = []
+_big_error = []
 
 i = 0
 
 def fuckin_freud(l, w, R, beta, dims):
 
     a, b, c, d = dims
-
-    # print('a={}, b={}, c={} and d={}'.format(a, b, c, d))
 
     theta_input = float(atan(l/(R-w/2)))
     theta_output_ackerman = float(acot(cot(theta_input) + w/l))
@@ -32,11 +45,22 @@ def fuckin_freud(l, w, R, beta, dims):
 
     theta_output_freud = freudenstein_eq(theta_input)
     error = (theta_output_freud-theta_output_ackerman)
-    # print('Radius: {} / Ackerman: {:.5f} / Freudenstein: {:.5f} / Error: {:.5f}'.format(R, degrees(theta_output_ackerman), degrees(theta_output_freud), degrees(error)))
 
     return error, theta_input, theta_output_ackerman, theta_output_freud
 
 def freud_opter(dims):
+
+    global _index
+    global _radius
+    global _input
+    global _ackerman
+    global _freudenstein
+    global _error
+    global _a
+    global _b
+    global _beta
+    global _d
+    global _big_error
 
     global i
     i += 1
@@ -48,62 +72,46 @@ def freud_opter(dims):
     l = 2.93
     w = 1.5
 
-    _radius = []
-    _input = []
-    _ackerman = []
-    _freudenstein = []
-    _error = []
-
-    _a = []
-    _b = []
-    _beta = []
-    _d = []
-
-    _index = []
-    _big_error = []
+    _radius_inst = []
+    _input_inst = []
+    _ackerman_inst = []
+    _freudenstein_inst = []
+    _error_inst = []
 
     big_error = 0
     for radius in range(3, 201):
         error, theta_input, theta_output_ackerman, theta_output_freud = fuckin_freud(l, w, radius, beta, (a, b, c, d))
 
-        _radius = np.append(_radius, radius)
-        _input = np.append(_input, theta_input)
-        _ackerman = np.append(_ackerman, theta_output_ackerman)
-        _freudenstein = np.append(_freudenstein, theta_output_freud)
-        _error = np.append(_error, error)
-
-        _a = np.append(_a, a)
-        _b = np.append(_b, b)
-        _d = np.append(_d, d)
-        _beta = np.append(_beta, beta)
+        _radius_inst.append(radius)
+        _input_inst.append(theta_input)
+        _ackerman_inst.append(theta_output_ackerman)
+        _freudenstein_inst.append(theta_output_freud)
+        _error_inst.append(error)
 
         big_error = big_error + (error)**2
 
     print('For a={:.7f}, b={:.7f}, c={:.7f} and d={:.7f}, beta={:.7f}, error={:.5f}.'.format(a, b, c, d, beta, big_error))
 
-    _index = np.append(_index, i)
-    _big_error = np.append(_big_error, big_error)
+    _radius.append(_radius_inst)
+    _input.append(_input_inst)
+    _ackerman.append(_ackerman_inst)
+    _freudenstein.append(_freudenstein_inst)
+    _error.append(_error_inst)
 
-    np.save('data/_radius_{}'.format(i), _radius)
-    np.save('data/_input_{}'.format(i), _input)
-    np.save('data/_ackerman_{}'.format(i), _ackerman)
-    np.save('data/_freudenstein_{}'.format(i), _freudenstein)
-    np.save('data/_error_{}'.format(i), _error)
-    np.save('data/_a_{}'.format(i), _a)
-    np.save('data/_b_{}'.format(i), _b)
-    np.save('data/_d_{}'.format(i), _d)
-    np.save('data/_beta_{}'.format(i), _beta)
-    np.save('data/_index_{}'.format(i), _index)
-    np.save('data/_big_error_{}'.format(i), _big_error)
+    _a.append(a)
+    _b.append(b)
+    _d.append(d)
+    _beta.append(beta)
+
+    _index.append(i)
+    _big_error.append(big_error)
 
     return big_error
 
-a = 0.11
-beta = 0.174
+a = 0.20
+beta = 0.43
 d = 1.45
 
-bnds = ((0.1, 0.3), (0.05, 0.5), (1.3, 1.5))
+bnds = ((0.05, 0.3), (0.05, 0.5), (1.3, 1.5))
 
 res = opt.minimize(freud_opter, (a, beta, d), bounds=bnds, method='SLSQP')
-
-
